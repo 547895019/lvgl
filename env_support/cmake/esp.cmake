daafdaf4b087fbc1b07a8370d511370479d93ea6
547895019 <547895019@qq.com>
2024-10-14 10:28:44 +0800

    修改cmake

--- env_support/cmake/esp.cmake
+++ env_support/cmake/esp.cmake
@@ -39,11 +39,17 @@ else()
     list(APPEND DEMO_SOURCES ${DEMO_MUSIC_SOURCES})
     set_source_files_properties(${DEMO_MUSIC_SOURCES} COMPILE_FLAGS "-Wno-format")
   endif()
-
+  if(CONFIG_LV_CONF_SUPPORT_WASM)
   idf_component_register(SRCS ${SOURCES} ${EXAMPLE_SOURCES} ${DEMO_SOURCES}
       INCLUDE_DIRS ${LVGL_ROOT_DIR} ${LVGL_ROOT_DIR}/src ${LVGL_ROOT_DIR}/../
                    ${LVGL_ROOT_DIR}/examples ${LVGL_ROOT_DIR}/demos
       REQUIRES esp_timer wasmachine)
+  else()
+  idf_component_register(SRCS ${SOURCES} ${EXAMPLE_SOURCES} ${DEMO_SOURCES}
+      INCLUDE_DIRS ${LVGL_ROOT_DIR} ${LVGL_ROOT_DIR}/src ${LVGL_ROOT_DIR}/../
+                   ${LVGL_ROOT_DIR}/examples ${LVGL_ROOT_DIR}/demos
+      REQUIRES esp_timer)
+  endif()
 endif()
 
 target_compile_definitions(${COMPONENT_LIB} PUBLIC "-DLV_CONF_INCLUDE_SIMPLE")
